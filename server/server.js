d2ad1d73d68d8a94bbd659f05c692d2067767d9a
Akshay Joy <akshayjoyinfo@gmail.com>
2019-12-03 00:16:47 +0530

    added Socket.io connect and diconnect

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require("express");
+const socketIO = require("socket.io");
+
 
 const publicPath = path.join(__dirname,"/../public");
 const PORT = process.env.PORT || 3000;
 
 let app = express();
-
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on("connection", (socket)=>{
+    console.log("A New User just Connected");
+    
+    socket.on("disconnect", (socket)=>{
+        console.log("user was disconnected");
+    })
+});
 
-app.listen(PORT, ()=>{
+
+
+server.listen(PORT, ()=>{
     console.log(`Server is UP on ${PORT} http://localhost:${PORT}`);
 })
